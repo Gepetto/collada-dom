6161c5df1c6378c01b6b6b2549e358fc03b90133
Guilhem Saurel <guilhem.saurel@laas.fr>
2026-07-29 12:53:52 +0200

    nix: switch to flakoboros

--- flake.nix
+++ flake.nix
@@ -1,44 +1,31 @@
 {
   description = "COLLADA Document Object Model (DOM) C++ Library";
 
-  inputs = {
-    flake-parts.url = "github:hercules-ci/flake-parts";
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-  };
+  inputs.gepetto.url = "github:gepetto/nix";
 
   outputs =
     inputs:
-    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = inputs.nixpkgs.lib.systems.flakeExposed;
-      perSystem =
-        {
-          lib,
-          pkgs,
-          self',
-          ...
-        }:
-        {
-          packages = {
-            default = self'.packages.collada-dom;
-            collada-dom = pkgs.collada-dom.overrideAttrs {
-              src = lib.fileset.toSource {
-                root = ./.;
-                fileset = lib.fileset.unions [
-                  ./cmake-modules
-                  ./CMakeLists.txt
-                  ./collada-dom-141.pc.in
-                  ./collada-dom-150.pc.in
-                  ./collada-dom.pc.in
-                  ./collada_dom-config-version.cmake.in
-                  ./collada_dom-config.cmake.in
-                  ./description
-                  ./dom
-                  ./licenses
-                  ./release
-                ];
-              };
-            };
+    inputs.gepetto.lib.mkFlakoboros inputs (
+      { lib, ... }:
+      {
+        overrideAttrs.collada-dom = {
+          src = lib.fileset.toSource {
+            root = ./.;
+            fileset = lib.fileset.unions [
+              ./cmake-modules
+              ./CMakeLists.txt
+              ./collada-dom-141.pc.in
+              ./collada-dom-150.pc.in
+              ./collada-dom.pc.in
+              ./collada_dom-config-version.cmake.in
+              ./collada_dom-config.cmake.in
+              ./description
+              ./dom
+              ./licenses
+              ./release
+            ];
           };
         };
-    };
+      }
+    );
 }
